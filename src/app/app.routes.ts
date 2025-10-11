f87f1c0fc311641708900077cda12c3adfb9af06
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-10-11 13:58:59 +0100

    produit c  creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Produits as ProduitsComponent } from './produits/produits';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: "produits", component : ProduitsComponent}
+
+];
