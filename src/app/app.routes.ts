5bb4d8d2e153349b8f8271e0efa8d9f01947f539
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-10-11 14:01:31 +0100

    add-produit c  creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { Produits as ProduitsComponent } from './produits/produits';
+import { AddProduit as AddProduitComponent } from './add-produit/add-produit';
 
 
 export const routes: Routes = [
-    {path: "produits", component : ProduitsComponent}
+    {path: "produits", component : ProduitsComponent},
+    {path: "add-produit", component : AddProduitComponent}
+
 
 ];
