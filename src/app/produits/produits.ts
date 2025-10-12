d1e9854e1aecd74e956e7040f2fc8f407c3670e9
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-10-12 16:10:08 +0100

    creation model produit

--- src/app/produits/produits.ts
+++ src/app/produits/produits.ts
@@ -1,17 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
 
 @Component({
   selector: 'app-produits',
   imports: [CommonModule],
   templateUrl: './produits.html',
 })
-export class Produits {
-  produits : string[];
+export class Produits implements OnInit {
+  produits: Produit[] = []; // Array of Produit objects
+
   constructor() {
-  this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"];
-}
+    this.produits = [
+      {
+        idProduit: 1,
+        nomProduit: "PC Asus",
+        prixProduit: 3000.600,
+        dateCreation: new Date("01/14/2011")
+      },
+      {
+        idProduit: 2,
+        nomProduit: "Imprimante Epson",
+        prixProduit: 450,
+        dateCreation: new Date("12/17/2010")
+      },
+      {
+        idProduit: 3,
+        nomProduit: "Tablette Samsung",
+        prixProduit: 900.123,
+        dateCreation: new Date("02/20/2020")
+      }
+    ];
+  }
 
-
-
+  ngOnInit(): void {
+  }
 }
