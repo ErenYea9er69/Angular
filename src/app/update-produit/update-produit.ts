c1cde613144aaa9e03014ba954d4b74a59b6126f
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-10-16 17:15:22 +0100

    Ajouter le champ catégorie au formulaire de modification

--- src/app/update-produit/update-produit.ts
+++ src/app/update-produit/update-produit.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ProduitService } from '../services/produit';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Categorie } from '../model/categorie.model';
+
 @Component({
   selector: 'app-update-produit',
   standalone: true,
@@ -13,18 +15,26 @@ import { CommonModule } from '@angular/common';
 export class UpdateProduit implements OnInit {
 
   currentProduit = new Produit();
+  categories! : Categorie[];
+  updatedCatId! : number;
+
   constructor(private activatedRoute: ActivatedRoute,
     private router :Router,
      private produitService: ProduitService) {}
   ngOnInit() {
     // console.log(this.route.snapshot.params.id);
+    this.categories = this.produitService.listeCategories();
+
     this.currentProduit = this.produitService.consulterProduit(
-      this.activatedRoute.snapshot.params['id']
-    );
+      this.activatedRoute.snapshot.params['id']);
+    this.updatedCatId=this.currentProduit.categorie.idCat;
+
     console.log(this.currentProduit);
   }
 updateProduit()
 { //console.log(this.currentProduit);
+  this.currentProduit.categorie=this.produitService.consulterCategorie(this.updatedCatId);
+
 this.produitService.updateProduit(this.currentProduit);
 this.router.navigate(['produits']);
 
