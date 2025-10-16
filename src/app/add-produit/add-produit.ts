b5f0f919394d311b9e608bccc316df1ff2ce4e6c
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-10-16 17:05:07 +0100

    Ajouter le champ catégorie au formulaire d’ajout

--- src/app/add-produit/add-produit.ts
+++ src/app/add-produit/add-produit.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Produit } from '../model/produit.model';
 import { ProduitService } from '../services/produit';
+import { Categorie } from '../model/categorie.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-produit',
@@ -9,21 +11,28 @@ import { ProduitService } from '../services/produit';
   imports: [FormsModule],
   templateUrl: './add-produit.html',
 })
-export class AddProduitComponent {
-
+export class AddProduitComponent implements OnInit {
   newProduit: Produit = new Produit();
-
+  categories! : Categorie[];
+  newIdCat! : number;
+  newCategorie! : Categorie;
   message: string = '';
 
-  constructor(private produitService: ProduitService) {}
+  constructor(private produitService: ProduitService, private router: Router ) {}
+
+  ngOnInit(): void {
+    this.categories = this.produitService.listeCategories();
+  }
 
   addProduit() {
+
+    this.newCategorie = this.produitService.consulterCategorie(this.newIdCat);
+    this.newProduit.categorie = this.newCategorie;
     if (this.newProduit.idProduit && this.newProduit.nomProduit && this.newProduit.prixProduit) {
       this.produitService.ajouterProduit(this.newProduit);
       this.message = "Produit " + this.newProduit.nomProduit + " ajouté avec succès !";
-      
-      // Clear the form
-      this.newProduit = new Produit();
-      
+      this.router.navigate(['produits']);
 
-}}}
+    }
+  }
+}
--- src/app/services/produit.ts
+++ src/app/services/produit.ts
@@ -8,39 +8,37 @@ import { Categorie } from '../model/categorie.model';
 export class ProduitService {
   produits: Produit[];
   produit!: Produit;
-  categories! : Categorie[];
+  categories!: Categorie[];
   //un tableau de Produit
   constructor() {
+    this.categories = [
+      { idCat: 1, nomCat: 'PC' },
+      { idCat: 2, nomCat: 'Imprimante' },
+    ];
 
-
-    this.categories = [ {idCat : 1, nomCat : "PC"},
-                        {idCat : 2, nomCat : "Imprimante"}];
-
-    
-this.produits = [
-  {
-    idProduit: 1,
-    nomProduit: 'PC Asus',
-    prixProduit: 3000.6,
-    dateCreation: new Date('01/14/2011'),
-    categorie: { idCat: 1, nomCat: 'PC' },
-  },
-  {
-    idProduit: 2,
-    nomProduit: 'Imprimante Epson',
-    prixProduit: 450,
-    dateCreation: new Date('12/17/2010'),
-    categorie: { idCat: 2, nomCat: 'Imprimante' },
-  },
-  {
-    idProduit: 3,
-    nomProduit: 'Tablette Samsung',
-    prixProduit: 900.123,
-    dateCreation: new Date('02/20/2020'),
-    categorie: { idCat: 1, nomCat: 'PC' },
-  },
-];
-
+    this.produits = [
+      {
+        idProduit: 1,
+        nomProduit: 'PC Asus',
+        prixProduit: 3000.6,
+        dateCreation: new Date('01/14/2011'),
+        categorie: { idCat: 1, nomCat: 'PC' },
+      },
+      {
+        idProduit: 2,
+        nomProduit: 'Imprimante Epson',
+        prixProduit: 450,
+        dateCreation: new Date('12/17/2010'),
+        categorie: { idCat: 2, nomCat: 'Imprimante' },
+      },
+      {
+        idProduit: 3,
+        nomProduit: 'Tablette Samsung',
+        prixProduit: 900.123,
+        dateCreation: new Date('02/20/2020'),
+        categorie: { idCat: 1, nomCat: 'PC' },
+      },
+    ];
   }
   listeProduits(): Produit[] {
     return this.produits;
@@ -67,11 +65,21 @@ this.produits.splice(index, 1);
     return this.produit;
   }
   updateProduit(prod: Produit) {
-    //chercher le produit prod du tableau produits
     const index = this.produits.indexOf(prod, 0);
     if (index > -1) {
-      this.produits.splice(index, 1); //supprimer l'ancien éléments
-      this.produits.splice(index, 0, prod); // insérer le nouvel élément
+      this.produits.splice(index, 1);
+      this.produits.splice(index, 0, prod);
     }
   }
+
+  listeCategories(): Categorie[] {
+    return this.categories;
+  }
+  consulterCategorie(id: number): Categorie {
+    return this.categories.find((cat) => cat.idCat == id)!;
+  }
+
+
+
+  
 }
