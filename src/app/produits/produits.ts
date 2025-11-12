3620b4bf7a62867394cf27f44398ce1570699020
ErenYea9er69 <rayenfootball.24@gmail.com>
2025-11-12 18:47:23 +0100

    it load at first

--- src/app/produits/produits.ts
+++ src/app/produits/produits.ts
@@ -1,44 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { Produit } from '../model/produit.model';
 import { ProduitService } from '../services/produit';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-produits',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './produits.html',
 })
 export class Produits implements OnInit {
-  produits: Produit[] = [];
+  produits = signal<Produit[]>([]);
 
+  constructor(private produitService: ProduitService) {}
 
-constructor(private produitService : ProduitService) {}
+  ngOnInit(): void {
+    this.chargerProduits();
+  }
 
-
-
-ngOnInit(): void {
-  this.chargerProduits();
-}
-
-chargerProduits(){
-  this.produitService.listeProduit().subscribe(prods => {
-    console.log(prods);
-    this.produits = prods;
-  });
-}
-
-supprimerProduit(p: Produit)
-{
-  let conf = confirm("Etes-vous sûr ?");
-  if (conf)
-    this.produitService.supprimerProduit(p.idProduit).subscribe(() => {
-      console.log("produit supprimé");
-      this.chargerProduits();
+  chargerProduits() {
+    this.produitService.listeProduit().subscribe(prods => {
+      console.log(prods);
+      this.produits.set(prods);
     });
-} 
+  }
 
-
-
+  supprimerProduit(p: Produit) {
+    let conf = confirm("Etes-vous sûr ?");
+    if (conf)
+      this.produitService.supprimerProduit(p.idProduit).subscribe(() => {
+        console.log("produit supprimé");
+        this.chargerProduits();
+      });
+  }
 }
